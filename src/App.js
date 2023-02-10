96674fe5a36ebb915f16d8f4d3b3a89a1e7ec2c0
jeffwang <wangjeffrey30@gmail.com>
2023-02-09 23:59:12 -0500

    app update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+
 function App() {
   return (
     <>
-    <h1>Mississauga Housing Marketplace</h1>
+    <Router>
+      <Route path='/' element={<Explore/>}
+      <Route path='/' element={<Explore/>}
+      <Route path='/' element={<Explore/>}
+      <Route path='/' element={<Explore/>}
+      <Route path='/' element={<Explore/>}
+      <Route path='/' element={<Explore/>}
+      </Routes>
+    </Router>
+    {/* Navbar */}
     </>
   )
 }
