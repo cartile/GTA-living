97e7ed4b3fbfb1458cf10e15a5414df612491439
jeffwang <wangjeffrey30@gmail.com>
2023-02-12 00:49:11 -0500

    nvbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {useNavigate, useLocation} from 'react-router-dom'
+import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
+import {ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg'
+import {ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
+
+
+function Navbar() {
+  return (
+    <footer className='navbar'>
+        <nav className="navbarBav">
+            <ul className="navbarListItems">
+                <li className="navbarListItem">
+                    
+                </li>
+            </ul>
+        </nav>
+
+    </footer>
+  )
+}
+
+export default Navbar
