a7943af17891a838a21d3bac9a5ac517c80b8f8f
jeffwang <wangjeffrey30@gmail.com>
2023-02-15 00:21:13 -0500

    routing config

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 
+import Navbar from "./components/Navbar"
+
 function App() {
   return (
     <>
     <Router>
       <Routes>
-      <Route path='/' element={<Explore />} />
-      <Route path='/offers' element={<Offers />} />
-      <Route path='/profile' element={<SignIn />} />
-      <Route path='/sign-in' element={<SignIn />} />
-      <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<SignIn />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
       </Routes>
+      <Navbar />
     </Router>
-    {/* Navbar */}
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
-import {useNavigate, useLocation} from 'react-router-dom'
+import {useNavigate, useLocation, Route} from 'react-router-dom'
 import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
 import {ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg'
 import {ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
 
 
 function Navbar() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const pathMatchRoute = (route) => {
+    if(route==location.pathname) {
+      return true
+    }
+  }
+
   return (
     <footer className='navbar'>
-        <nav className="navbarBav">
+        <nav className="navbarNav">
             <ul className="navbarListItems">
-                <li className="navbarListItem">
+                <li className="navbarListItem" onClick={() => navigate ('/')}>
                     <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
                     <p>Explore</p>
                 </li>
-                <li className="navbarListItem">
+                <li className="navbarListItem" onClick={() => navigate ('/offers')}>
                     <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
                     <p>Offer</p>
                 </li>
-                <li className="navbarListItem">
+                <li className="navbarListItem" onClick={() => navigate ('/profile')}>
                     <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
                     <p>Profiles</p>
                 </li>
