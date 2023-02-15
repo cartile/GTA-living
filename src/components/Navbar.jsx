df6c42164f9e8b0e6a35c96fc9b172212a0a4851
jeffwang <wangjeffrey30@gmail.com>
2023-02-14 23:57:42 -0500

    profiles/offer icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,19 @@ function Navbar() {
         <nav className="navbarBav">
             <ul className="navbarListItems">
                 <li className="navbarListItem">
-                    
+                    <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
+                    <p>Explore</p>
+                </li>
+                <li className="navbarListItem">
+                    <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
+                    <p>Offer</p>
+                </li>
+                <li className="navbarListItem">
+                    <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
+                    <p>Profiles</p>
                 </li>
             </ul>
         </nav>
-
     </footer>
   )
 }
