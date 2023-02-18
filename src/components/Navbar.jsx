f4d0821dc78f38097d198d418dabfa848c92d9f7
jeffwang <wangjeffrey30@gmail.com>
2023-02-18 01:58:09 -0500

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,36 @@ function Navbar() {
   const navigate = useNavigate()
   const location = useLocation()
 
+  const pathMatchRoute = (route) => {
+    if(route==location.pathname) {
+      return true
+    }
+  }
 
   return (
     <footer className='navbar'>
         <nav className="navbarNav">
             <ul className="navbarListItems">
                 <li className="navbarListItem" onClick={() => navigate ('/')}>
-                    <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
-                    <p>Explore</p>
+                    <ExploreIcon fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
+                    <p className={pathMatchRoute('/')
+                     ? 'navbarListItemNameActive'
+                     : 'navbarListItemName'}
+                    >Explore</p>
                 </li>
                 <li className="navbarListItem" onClick={() => navigate ('/offers')}>
-                    <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
-                    <p>Offer</p>
+                    <OfferIcon fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
+                    <p className={pathMatchRoute('/offers')
+                     ? 'navbarListItemNameActive'
+                     : 'navbarListItemName'}
+                    >Offers</p>
                 </li>
                 <li className="navbarListItem" onClick={() => navigate ('/profile')}>
-                    <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
-                    <p>Profiles</p>
+                    <PersonOutlineIcon fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
+                    <p className={pathMatchRoute('/profile')
+                     ? 'navbarListItemNameActive'
+                     : 'navbarListItemName'}
+                    >Profile</p>
                 </li>
             </ul>
         </nav>
