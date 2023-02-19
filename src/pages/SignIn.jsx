721488297ea554640a7bca8fd6a455ff6a99bc70
jeffwang <wangjeffrey30@gmail.com>
2023-02-19 03:15:29 -0500

    signin logic

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,17 @@
+import {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SignIn () {
+    const [showPassword, setShowPassword] = useState(false)
+    const [formData, sestFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const navigate = useNavigate()
+
     return (
         <div>
             <h1>SignIn</h1>
