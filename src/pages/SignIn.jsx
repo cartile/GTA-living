899312fbd68d69d5fa5d9612342f954e1a4312b9
jeffwang <wangjeffrey30@gmail.com>
2023-02-21 00:48:06 -0500

    signIn

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,12 +5,21 @@ import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignIn () {
     const [showPassword, setShowPassword] = useState(false)
-    const [formData, sestFormData] = useState({
+    const [formData, setFormData] = useState({
         email: '',
         password: '',
     })
+    const { email, password } = FormData
 
     const navigate = useNavigate()
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+
+        }))
+    }
 
     return (
         <>
@@ -18,6 +27,49 @@ function SignIn () {
                 <header>
                     <p className="pageHeader">Welcome Back!</p>
                 </header>
+
+                <form>
+                    <input type="email" 
+                    className="emailInput" 
+                    placeholder="Email" 
+                    id="email" 
+                    value={email} 
+                    onChange={onChange}
+                    />
+
+                        <div className="passwordInputDiv">
+                            <input type ={showPassword ? "text" : "password"} 
+                            className="passwordInput"
+                            placeholder="Password" 
+                            id="password"
+                            value = {password}
+                            onChange={onChange}
+                            />
+
+                            <img src={visibilityIcon} 
+                            alt="show password"
+                             className="showPassword" 
+                             onClick={() => setShowPassword((prevState) => !prevState)}/>
+                        </div>
+
+                        <Link to='/forgot-password' className="forgotPasswordLink">
+                            Forgot Password
+                        </Link>
+
+                        <div className="signInBar">
+                            <p className="signInText">
+                                Sign In
+                            </p>
+                            <button className="signInButton">
+                                <ArrowRightIcon fill="#FFFFFF" width="34px" height= "34px"/>
+                            </button>
+                        </div>
+                    </form>
+
+                    {/* OAuth */}
+                    <Link to='/sign-up' className="registerLink">
+                        Sign Up Instead?
+                        </Link>
             </div>
         </>
     )
