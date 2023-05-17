439c47051efbede3bf75bf43514c4c76df40b7b8
jeffwang <wangjeffrey30@gmail.com>
2023-05-17 19:52:50 -0400

    signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,9 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile}
+from 'firebase/auth'
+import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
+import { db } from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -21,6 +25,30 @@ function SignUp () {
 
         }))
     }
+    
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const auth = getAuth()
+            const userCredential = await createUserWithEmailAndPassword
+            (auth, email, password)
+
+            const user = userCredential.user
+            updateProfile(auth.currentUser, { 
+                displayName: name
+            })
+            const formDataCopy = {...formData}
+            delete formDataCopy.password
+            formDataCopy.timestamp = serverTimestamp()
+
+            await setDoc(doc(db, 'users', user))
+
+            navigate('/')
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
 
     return (
         <>
@@ -29,7 +57,7 @@ function SignUp () {
                     <p className="pageHeader">Welcome Back!</p>
                 </header>
 
-                <form>
+                <form onSubmit={onSubmit}>
                 <input type="text" 
                     className="nameInput" 
                     placeholder="Name" 
