20cca0037ee9d714206f8d1ccdd66329cd635d6d
jeffwang <wangjeffrey30@gmail.com>
2023-05-18 17:06:36 -0400

    signup touchup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -34,14 +34,16 @@ function SignUp () {
             (auth, email, password)
 
             const user = userCredential.user
+
             updateProfile(auth.currentUser, { 
                 displayName: name
             })
+
             const formDataCopy = {...formData}
             delete formDataCopy.password
             formDataCopy.timestamp = serverTimestamp()
 
-            await setDoc(doc(db, 'users', user))
+            await setDoc(doc(db, 'users', user.uid), formDataCopy)
 
             navigate('/')
         } catch (error) {
