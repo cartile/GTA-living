0442d97e8545ac505d119ffaaaa3513f4c027005
jeffwang <wangjeffrey30@gmail.com>
2023-05-24 18:24:14 -0400

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,16 +3,15 @@ import { getAuth } from 'firebase/auth'
 
 
 function Profile () {
+
+    const[user, setUser] = useState(null)
+    const auth = getAuth()
+
     useEffect(() => {
-        console.log(auth.currentUser)
+        setUser(auth.currentUser)
     }, []) 
 
-    return (
-        <div>
-            <h1>Profile</h1>
-
-        </div>
-    )
+    return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
 }
 
 export default Profile
