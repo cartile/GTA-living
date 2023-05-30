763ce5e90f334413e6d65ac76aa897eef04ca9ef
jeffwang <wangjeffrey30@gmail.com>
2023-05-30 16:52:53 -0400

    spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className='loadingSpinningContainer'>
+        <div className="loadingSpinner"></div>
+    </div>
+  )
+}
+
+export default Spinner
