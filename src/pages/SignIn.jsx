0003c5c81109392e4672163090a5acf325222f75
jeffwang <wangjeffrey30@gmail.com>
2023-05-31 15:21:21 -0400

    google signin (Oauth)

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from '../components/OAuth'
 
 function SignIn () {
     const [showPassword, setShowPassword] = useState(false)
@@ -88,7 +89,7 @@ function SignIn () {
                         </div>
                     </form>
 
-                    {/* OAuth */}
+                    <OAuth />
                     <Link to='/sign-up' className="registerLink">
                         Sign Up Instead?
                         </Link>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,6 +7,7 @@ import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
 import { db } from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from '../components/OAuth'
 
 function SignUp () {
     const [showPassword, setShowPassword] = useState(false)
@@ -105,7 +106,7 @@ function SignUp () {
                         </div>
                     </form>
 
-                    {/* OAuth */}
+                    <OAuth />
                     <Link to='/sign-in' className="registerLink">
                         Sign In Instead?
                         </Link>
