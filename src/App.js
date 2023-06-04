b1e09046a3e272fd70bec43bdfb70447398f3486
jeffwang <wangjeffrey30@gmail.com>
2023-06-04 15:33:52 -0400

    explore categories

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Offers from './pages/Offers'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Category from './pages/Category'
 import ForgotPassword from './pages/ForgotPassword'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Explore />} />
         <Route path='/offers' element={<Offers />} />
+        <Route path='/category/:categoryName' element={<Category />} />
         <Route path='/profile' element={<PrivateRoute />} >
             <Route path='/profile' element={<Profile />} />
         </Route>
