b67bd99ddcc4e22adae2890902d32baa55ae2011
jeffwang <wangjeffrey30@gmail.com>
2023-06-14 02:29:09 -0400

    listing data fetch

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,10 +8,26 @@ import shareIcon from '../assets/svg/shareIcon.svg'
 
 function Listing() {
     const [listing, setListing] = useState(null)
-    const [loading, setLoading] = useState(null)
-    const [shareLinkCopied, setShareLinkCopied] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
-    
+    const navigate = useNavigate()
+    const params = useParams()
+    const auth = getAuth()
+
+    useEffect(() => {
+        const fetchListing = async () =>{
+            const docRef = doc(db, 'listings', params.listingId)
+            const docSnap = await getDoc(docRef)
+
+            if(docSnap.exists()) {
+                console.log(docSnap.data())
+                setListing(docSnap.data())
+                setLoading(false)
+            }
+        }
+        fetchListing()
+    }, [navigate, params.listingId])
 
 
     return (
