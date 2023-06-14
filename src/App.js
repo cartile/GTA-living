df65a482c5bce58ef393bfb123b1ace0d4053a2a
jeffwang <wangjeffrey30@gmail.com>
2023-06-14 18:22:03 -0400

    contact landlord functionality

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import Category from './pages/Category'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "./components/Navbar"
 import PrivateRoute from "./components/PrivateRoute"
+
 
 function App() {
   return (
@@ -29,6 +31,7 @@ function App() {
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/create-listing' element={<CreateListing />} />
         <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+        <Route path='/contact/:landlordId' element={<Contact />} />
       </Routes>
       <Navbar />
     </Router>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,60 @@
+import {useState, useEffect} from 'react'
+import {useParams, useSearchParams} from 'react-router-dom'
+import {doc, getDoc} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
+
+function Contact() {
+    const [message, setMessage] = useState('')
+    const [landlord, setLandlord] = useState(null)
+    const [searchParams, setSearchParams] = useSearchParams()
+    
+    const params = useParams()
+    useEffect(() => {
+        const getLandlord = async () => {
+            const docRef = doc(db, 'users', params.landlordId)
+            const docSnap = await getDoc(docRef)
+
+            if(docSnap.exists()) {
+                setLandlord(docSnap.data())
+            } else {
+                toast.error('Could not get landlord data')
+            }
+
+        }
+        getLandlord()
+    }, [params.landlordId])
+
+    const onChange = e => setMessage(e.target.value)
+    const listingName = searchParams.get('listingName');
+    return (
+    <div className='pageContainer'>
+        <header>
+            <p className='pageHeader'>Contact Landlord</p>
+        </header>
+        
+        {landlord !== null && (
+            <main>
+                <div className="contactLandlord">
+                    <p className="landlordName">Contact {landlord?.name}</p>
+                </div>
+                <form className="messageForm">
+                    <div className="messageDiv">
+                    <label htmlFor="message" className="messageLabel">
+                        Message
+                    </label>
+                    <textarea name="message" id="message" className='textarea' value={message} onChange={onChange}>
+                    </textarea>
+                    </div>
+                    
+                    <a href={`mailto:${landlord.email}?Subject=${encodeURIComponent(`${listingName} inquiry.`)}&body=${encodeURIComponent(message)}`}>
+                        <button type='button' className='primaryButton'>Send Message</button>
+                    </a>
+                </form>
+            </main>
+        )}
+    </div>
+    )
+}
+
+export default Contact
