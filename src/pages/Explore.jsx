f4765749be1d2e54bdfae013a667334cc63699f8
jeffwang <wangjeffrey30@gmail.com>
2023-06-15 19:31:13 -0400

    explore page recommended

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+import Slider from '../components/Slider'
 
 function Explore () {
     return (
@@ -10,7 +11,7 @@ function Explore () {
             </header>
 
             <main>
-                {/* Slider */}
+                <Slider/>
             </main>
 
             <p className="exploreCategoryHeading">Categories</p>
